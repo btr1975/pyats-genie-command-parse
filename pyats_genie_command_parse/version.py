1b6498032ec1d510e8d64f176d28a560c715a5e9
Ben <btr1975@gmail.com>
2023-07-30 16:07:49 -0500

    updated requirements-dev, and changed version to 1.3.7

--- pyats_genie_command_parse/version.py
+++ pyats_genie_command_parse/version.py
@@ -3,5 +3,5 @@ Holds the version information for the package
 """
 __copyright__ = "Copyright (c) 2020 - 2023, Benjamin P. Trachtenberg, Brett Gianpetro"
 __status__ = 'prod'
-__version_info__ = (1, 3, 6)
+__version_info__ = (1, 3, 7)
 __version__ = '.'.join(map(str, __version_info__))
