5a742259e657dbca2c11ae78d3a14654009c3eef
Ben <e_ben_75-python@yahoo.com>
2024-08-16 22:00:59 +0000

    Moved version to 1.3.11

--- pyats_genie_command_parse/version.py
+++ pyats_genie_command_parse/version.py
@@ -3,5 +3,5 @@ Holds the version information for the package
 """
 __copyright__ = "Copyright (c) 2020 - 2024, Benjamin P. Trachtenberg, Brett Gianpetro"
 __status__ = 'prod'
-__version_info__ = (1, 3, 10)
+__version_info__ = (1, 3, 11)
 __version__ = '.'.join(map(str, __version_info__))
