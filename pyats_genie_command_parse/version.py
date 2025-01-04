9f13dfd9afa39ad98f8af79ba82f0508e4996a8f
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2025-01-04 17:24:15 -0600

    update to genie 24.11 removed universal, added support for 3.13 remove supprt for 3.8 moved version to 1.4.0

--- pyats_genie_command_parse/version.py
+++ pyats_genie_command_parse/version.py
@@ -1,7 +1,7 @@
 """
 Holds the version information for the package
 """
-__copyright__ = "Copyright (c) 2020 - 2024, Benjamin P. Trachtenberg, Brett Gianpetro"
+__copyright__ = "Copyright (c) 2020 - 2025, Benjamin P. Trachtenberg, Brett Gianpetro"
 __status__ = 'prod'
-__version_info__ = (1, 3, 11)
+__version_info__ = (1, 4, 0)
 __version__ = '.'.join(map(str, __version_info__))
